6de025661ba824a552b7f2ccaf9b9d649a55607b
putri <antariputri26@gmail.com>
2024-03-09 21:58:26 +0800

    login page

--- login.php
+++ login.php
@@ -79,7 +79,8 @@
                     </div>
 
                     <!-- <div class="text-center p-t-12">
-              <span class="txt1"> Forgot </span>
+              <span class="txt1"> Forgot </span>git push -u origin main
+
               <a class="txt2" href="#"> Username / Password? </a>
             </div> -->
 
